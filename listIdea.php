79cd287d05fbef8b00e83c34d41f4f93f2271b9b
ZakariaLiveCampus <zakaria.saber@livecampus.me>
2024-11-15 21:28:31 +0100

    fonctionnalité vote : réussie, manque la factorisation du code + commentaire

--- listIdea.php
+++ listIdea.php
@@ -12,60 +12,71 @@ if (file_exists($fileIdea)) {
 }
 
 $fileVote = 'vote.json';
-$responseIdea = [];
 
 if (file_exists($fileVote)) {
     $fileVoteContent = file_get_contents($fileVote);
     $responseIdea = json_decode($fileVoteContent, true);
 };
 
-
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $response = filter_input(INPUT_POST, "response", FILTER_SANITIZE_SPECIAL_CHARS);
     $id_idea = filter_input(INPUT_POST, "id_idea", FILTER_SANITIZE_SPECIAL_CHARS);
 
+    // Vérification de l'ancien vote de l'utilisateur
+    $userVote = null; 
+    if(!empty($responseIdea)){
+        foreach ($responseIdea as $vote) {
+            if ($vote['id_idea'] === $id_idea && $vote['author'] === $_SESSION['username']) {
+                $userVote = $vote['response'];
+                break;
+            }
+        }
+    }
+
+    // Mettre à jour les compteurs avant d'ajouter le nouveau vote
+    if (!empty($ideasFichiers)) {
+        foreach ($ideasFichiers as $index => $oneIdea) {
+            if ($oneIdea["id"] === $id_idea) {
+                if ($response === "Aimer" && $userVote !== "Aimer") {    
+                    $ideasFichiers[$index]["likes"]++;
+                    if ($userVote === "Ne pas aimer") {
+                        $ideasFichiers[$index]["dislikes"] > 0 ? $ideasFichiers[$index]["dislikes"]-- : '';
+                    }
+                } elseif ($response === "Ne pas aimer" && $userVote !== "Ne pas aimer") {
+                    $ideasFichiers[$index]["dislikes"]++;
+                    if ($userVote === "Aimer") {
+                        $ideasFichiers[$index]["likes"] > 0 ? $ideasFichiers[$index]["likes"]-- : '';
+                    }
+                }
+                break; // Une fois l'idée trouvée, sortir de la boucle
+            }
+        }
+
+        // Sauvegarder les modifications dans le fichier idea.json
+        file_put_contents($fileIdea, json_encode($ideasFichiers, JSON_PRETTY_PRINT));
+    }
+
+    // Supprimer l'ancien vote de l'utilisateur dans responseIdea
     if (!empty($responseIdea)){
-        foreach($responseIdea as $index => $oneResponseIdea){
-            if($id_idea === $oneResponseIdea['id_idea']){
+        foreach ($responseIdea as $index => $oneResponseIdea) {
+            if ($id_idea === $oneResponseIdea["id_idea"] && $oneResponseIdea["author"] === $_SESSION["username"]) {
                 unset($responseIdea[$index]);
             }
         }
     };
+
+    // Ajouter le nouveau vote
     $votes = [
-        'id' => bin2hex(string: random_bytes(8)),
+        'id' => bin2hex(random_bytes(8)),
         'response' => $response,
         'author' => $_SESSION['username'],
         'id_idea' => $id_idea
     ];
-    var_dump($responseIdea);
-    
-    
     $responseIdea[] = $votes;
-    file_put_contents($fileVote, json_encode($responseIdea));
-    // Essayer de trouver comment éviter le spam de like/dislike par l'utilisateur
-    if (!empty($ideasFichiers)){
-        $reponseExistante = "";
-        foreach($ideasFichiers as $index => $oneIdea){
-            if ($oneIdea["id"] === $id_idea) {
-                if ($response === "Aimer") {    
-                    $ideasFichiers[$index]["likes"]++;
-                    $ideasFichiers[$index]["dislikes"] > 0 ? $ideasFichiers[$index]["dislikes"]-- : '';
-                
-                } elseif ($response === "Ne pas aimer") {
-                    $ideasFichiers[$index]["dislikes"]++;
-                    $ideasFichiers[$index]["likes"] > 0 ? $ideasFichiers[$index]["likes"]-- : '';
-                    $isUpdated = true;
-                }
-            }
-        }
-        $newJsonData = json_encode($ideasFichiers);
-        file_put_contents($fileIdea, $newJsonData);
-    };
 
-
-}
-
+    // Sauvegarder les votes dans le fichier vote.json
+    file_put_contents($fileVote, json_encode(array_values($responseIdea), JSON_PRETTY_PRINT));
+};
 
 ?>
 
@@ -115,6 +126,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <tbody>
             <?php if (!empty($ideasFichiers)): ?>
                 <?php foreach ($ideasFichiers as $idea): ?>
+                    <?php
+                    // Vérifier si l'utilisateur a déjà voté pour cette idée
+                    $userVote = null; 
+                    if (!empty($responseIdea)){
+                        foreach ($responseIdea as $vote) {
+                            if ($vote['id_idea'] === $idea['id'] && $vote['author'] === $_SESSION['username']) {
+                                $userVote = $vote['response']; // "Aimer" ou "Ne pas aimer"
+                                break;
+                            }
+                        }
+                    }
+                    ?>
                     <tr>
                         <td><?= htmlspecialchars($idea['title']) ?></td>
                         <td><?= htmlspecialchars($idea['description']) ?></td>
@@ -122,11 +145,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <td><?= htmlspecialchars($idea['createdAt']) ?></td>
                         <form method="POST">
                             <td>
-                                <button type="submit" name="response" value="Aimer">J'aime cette idée</button>
+                                <button type="submit" name="response" value="Aimer" <?= $userVote === "Aimer" ? 'disabled' : '' ?>>J'aime cette idée</button>
+                                <!-- <button type="submit" name="response" value="Aimer">J'aime cette idée</button> -->
                                 <p><?= htmlspecialchars($idea['likes']) ?></p>
                             </td>
                             <td>
-                                <button type="submit" name="response" value="Ne pas aimer">Je n'aime pas cette idée</button>
+                                <button type="submit" name="response" value="Ne pas aimer" <?= $userVote === "Ne pas aimer" ? 'disabled' : '' ?>>Je n'aime pas cette idée</button>
+                                <!-- <button type="submit" name="response" value="Ne pas aimer">Je n'aime pas cette idée</button> -->
                                 <p><?= htmlspecialchars($idea['dislikes']) ?></p>
                             </td>
                             <input type="hidden" name="id_idea" value="<?= htmlspecialchars($idea['id']) ?>">
